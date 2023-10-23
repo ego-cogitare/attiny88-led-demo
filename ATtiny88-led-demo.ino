dc7dc9772fbc5716f0f9116a361dcf64fa55730a
ego-cogitare <ego.cogitare@gmail.com>
2023-10-23 21:17:24 +0200

    Update ATtiny88-led-demo.ino

--- ATtiny88-led-demo.ino
+++ ATtiny88-led-demo.ino
@@ -1,4 +1,6 @@
 #define PINS 16
+#define MAX_DELAY 100
+#define MIN_DELAY 1
 
 void setup() {
   for (uint8_t i = 1; i <= PINS; i++) {
@@ -8,13 +10,28 @@ void setup() {
 
 uint8_t pin = 1;
 int8_t incr = 1;
+uint8_t del = MIN_DELAY;
+uint32_t i = 0;
+bool avers = true;
 
 void loop() {
     digitalWrite(pin, HIGH);
-    delay(40);
+    delay(del);
     digitalWrite(pin, LOW);
     pin += incr;
     if (pin == PINS || pin == 1) {
       incr = -incr;
+    }
+
+    i++;
+    if (i > pow(MAX_DELAY, 1.02) / pow(del, 1.02)) {
+      del += avers ? 1 : -1;
+      i = 0;
+    }
+
+    if (del == MAX_DELAY) {
+      avers = false;
+    } else if (del == MIN_DELAY) {
+      avers = true;
     }
 }
